77e477b249590a5b7e89dd69dd168386dc119d2d
Yu-Ming Chen <yminchen@umich.edu>
2019-03-04 16:53:49 -0500

    Commit before switching branch

---
+++ examples/Goldilocks_models/kinematics_expression_test.cc
@@ -0,0 +1,21 @@
+#include "examples/Goldilocks_models/kinematics_expression.h"
+
+
+
+namespace dairlib {
+namespace goldilocks_models {
+
+
+}  // namespace goldilocks_models
+}  // namespace dairlib
+
+using namespace dairlib::goldilocks_models;
+
+int main() {
+    int n_z = 4;
+    KinematicsExpression<double> expr(n_z);
+
+
+
+  return 0;
+}
