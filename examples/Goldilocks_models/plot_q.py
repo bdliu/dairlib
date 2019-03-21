e6bba2207c7ac8fdb414e1a793addf69ecae1bf8
Yu-Ming Chen <yminchen@umich.edu>
2019-03-21 12:14:41 -0400

    Modify plot q

--- examples/Goldilocks_models/plot_q.py
+++ examples/Goldilocks_models/plot_q.py
@@ -6,12 +6,15 @@ import time
 min_dist = 0.24
 delta_dist = 0.03
 
-iteration = 18
+
+iteration_start = 1
+iteration_end = 34
+
+iteration = 1
 batch = 2
 directory = 'data/'
 
 fig = plt.figure(1)
-ax = fig.gca()
 
 t = []
 if os.path.isfile(directory+str(iteration)+'_'+str(batch)+'_time_at_knots.csv'):
@@ -19,39 +22,54 @@ if os.path.isfile(directory+str(iteration)+'_'+str(batch)+'_time_at_knots.csv'):
         plots = csv.reader(csvfile, delimiter=',')
         for row in plots:
             t.append(row[0])
-for joint in range(7):
-    joint_angle = []
-    if os.path.isfile(directory+str(iteration)+'_'+str(batch)+'_state_at_knots.csv'):
-        with open(directory+str(iteration)+'_'+str(batch)+'_state_at_knots.csv','r') as csvfile:
+
+for iteration in range(iteration_start,iteration_end+1):
+    ax = fig.gca()
+    for joint in range(7):
+        joint_angle = []
+        if os.path.isfile(directory+str(iteration)+'_'+str(batch)+'_state_at_knots.csv'):
+            with open(directory+str(iteration)+'_'+str(batch)+'_state_at_knots.csv','r') as csvfile:
+                plots = csv.reader(csvfile, delimiter=',')
+                row_num = 0
+                for row in plots:
+                    if row_num == joint:
+                        joint_angle = row;
+                    row_num+=1
+        ax.plot(t,joint_angle, label='q('+str(joint)+')')
+        ax.tick_params(axis='x', labelsize=15)
+        ax.tick_params(axis='y', labelsize=15)
+
+    cost = []
+    if os.path.isfile(directory+str(iteration)+'_'+str(batch)+'_c.csv'):
+        with open(directory+str(iteration)+'_'+str(batch)+'_c.csv','r') as csvfile:
             plots = csv.reader(csvfile, delimiter=',')
-            row_num = 0
             for row in plots:
-                if row_num == joint:
-                    joint_angle = row;
-                row_num+=1
-    ax.plot(t,joint_angle, label='q('+str(joint)+')')
-    ax.tick_params(axis='x', labelsize=15)
-    ax.tick_params(axis='y', labelsize=15)
+                cost.append(row[0])
 
-plt.xlabel('t (s)', fontsize=15)
-plt.ylabel('q (m or rad)', fontsize=15)
-# plt.title('Generalized position trajectories')
-leg = plt.legend()
+    plt.xlabel('t (s)', fontsize=15)
+    plt.ylabel('q (m or rad)', fontsize=15)
+    # plt.title('Generalized position trajectories.')
+    plt.title('Iteration #'+str(iteration)+': cost = '+str(cost[0]))
+    leg = plt.legend()
 
 
-# Set the axis limit
-ax.set_xlim(0, 0.95)
-ax.set_ylim(-0.45, 1.1)
+    # Set the axis limit
+    ax.set_xlim(0, 0.95)
+    ax.set_ylim(-0.45, 1.1)
 
-# Draw the figure so you can find the positon of the legend.
-plt.draw()
+    # Draw the figure so you can find the positon of the legend.
+    plt.draw()
 
-# Get the bounding box of the original legend
-bb = leg.get_bbox_to_anchor().inverse_transformed(ax.transAxes)
+    # # Get the bounding box of the original legend
+    # bb = leg.get_bbox_to_anchor().inverse_transformed(ax.transAxes)
+    # # Change to location of the legend.
+    # bb.x0 += 0
+    # # bb.x1 += 0.12
+    # leg.set_bbox_to_anchor(bb, transform = ax.transAxes)
 
-# Change to location of the legend.
-bb.x0 += 0
-# bb.x1 += 0.12
-leg.set_bbox_to_anchor(bb, transform = ax.transAxes)
-
-plt.show()
+    if (iteration_start == iteration_end):
+        plt.show()
+    else:
+        plt.draw()
+        plt.pause(1)
+        plt.clf()
