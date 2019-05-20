e74fe8b1e6f3da019016ea44b24452ab97891478
Yu-Ming Chen <yminchen@umich.edu>
2019-05-19 21:29:34 -0400

    Update theta plotting script

--- examples/Goldilocks_models/plot_parameter.py
+++ examples/Goldilocks_models/plot_parameter.py
@@ -2,9 +2,26 @@ import matplotlib.pyplot as plt
 import csv
 import os
 import time
+import sys
 
-col_ploted = range(6) #68 or 6
+idx_start = 0
+idx_end = 6
+kin_or_dyn = 1
+if len(sys.argv) == 2:
+    idx_start = int(sys.argv[1])
+elif len(sys.argv) == 3:
+    idx_start = int(sys.argv[1])
+    idx_end = int(sys.argv[2])
+elif len(sys.argv) == 4:
+    idx_start = int(sys.argv[1])
+    idx_end = int(sys.argv[2])
+    kin_or_dyn = int(sys.argv[3])
+
+
+col_ploted = range(idx_start,idx_end)
 name = 'theta_sDDot' # theta_s or theta_sDDot
+if kin_or_dyn == 0:
+    name = 'theta_s'
 
 iter_start = 1
 directory = 'data/'
@@ -31,8 +48,8 @@ while 1:
 
     plt.xlabel('iterations')
     plt.ylabel('parameter values')
-    plt.title('parameter')
-    plt.legend()
+    plt.title(name)
+    # plt.legend()
     plt.draw()
 
     plt.pause(10)
