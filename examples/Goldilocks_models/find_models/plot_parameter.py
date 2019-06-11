b8608aac34d853dd1f3ca08bd0ca364eb2000341
Yu-Ming Chen <yminchen@umich.edu>
2019-06-11 11:15:45 -0400

    Fix a bug of a python script that plots model parameters over time

--- examples/Goldilocks_models/find_models/plot_parameter.py
+++ examples/Goldilocks_models/find_models/plot_parameter.py
@@ -3,6 +3,7 @@ import csv
 import os
 import time
 import sys
+import numpy as np
 
 idx_start = 0
 idx_end = 6
@@ -18,7 +19,7 @@ elif len(sys.argv) == 4:
     kin_or_dyn = int(sys.argv[3])
 
 
-col_ploted = range(idx_start,idx_end)
+row_idx_ploted = range(idx_start,idx_end)
 name = 'theta_sDDot' # theta_s or theta_sDDot
 if kin_or_dyn == 0:
     name = 'theta_s'
@@ -30,21 +31,16 @@ while 1:
     fig1 = plt.figure(1)
     ax1 = fig1.gca()
 
-    for col in col_ploted:
+    for row_idx in row_idx_ploted:
         theta_i = []
         iteration = iter_start
         while os.path.isfile(directory+str(iteration)+'_'+name+'.csv'):
-            with open(directory+str(iteration)+'_'+name+'.csv','r') as csvfile:
-                plots = csv.reader(csvfile, delimiter=',')
-                row_num = 0
-                for row in plots:
-                    if row_num == col:
-                        theta_i.append(row[0])
-                    row_num+=1
+            matrix = np.genfromtxt (directory+str(iteration)+'_'+name+'.csv', delimiter=",")
+            theta_i.append(matrix[row_idx])
             iteration+=1
         length = len(theta_i)
         t = range(iter_start,length+iter_start)
-        ax1.plot(t,theta_i, label=''+name+'('+str(col)+')')
+        ax1.plot(t,theta_i, label=''+name+'('+str(row_idx)+')')
 
     plt.xlabel('iterations')
     plt.ylabel('parameter values')
