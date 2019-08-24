6c07da156236b69bf8f78f8233a4414b40542100
Yu-Ming Chen <yminchen@umich.edu>
2019-08-24 02:15:06 -0400

    Adding constraint on force at collocation points doesn't improve the problem

--- systems/trajectory_optimization/hybrid_dircon.cc
+++ systems/trajectory_optimization/hybrid_dircon.cc
@@ -172,13 +172,13 @@ HybridDircon<T>::HybridDircon(
 
 
     //Force cost option
-    if (options[i].getForceCost() != 0) {
-      auto A = /*options[i].getForceCost()**/ 10 * MatrixXd::Identity(num_kinematic_constraints(i),num_kinematic_constraints(i));
-      auto b = MatrixXd::Zero(num_kinematic_constraints(i),1);
-      for (int j=0; j <  mode_lengths_[i]; j++) {
-        AddL2NormCost(A,b,force(i,j));
-      }
-    }
+    // if (options[i].getForceCost() != 0) {
+    //   auto A = /*options[i].getForceCost()**/MatrixXd::Identity(num_kinematic_constraints(i),num_kinematic_constraints(i));
+    //   auto b = MatrixXd::Zero(num_kinematic_constraints(i),1);
+    //   for (int j=0; j <  mode_lengths_[i]; j++) {
+    //     AddL2NormCost(A,b,force(i,j));
+    //   }
+    // }
 
     if (i > 0) {
       if (num_kinematic_constraints(i) > 0) {
